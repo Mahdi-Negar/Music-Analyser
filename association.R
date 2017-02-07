27a11d1c6222ff244927629f2eca65073dda3390
Negar Mokhberian <negar.mokhberian@gmail.com>
2017-02-07 19:05:46 +0330

    pink floyd rules esh khube

--- association.R
+++ association.R
@@ -2,9 +2,6 @@ library(Matrix)
 library(arules)
 library(arulesViz)
 library(colorspace)
-map_to_integer <- function(v){
-  as.integer(factor(v, levels = unique(v)))
-}
 
 transactions = as.data.frame(data_360k[,c(1,3)])
 transactions <- as(split(transactions[,"artname"], transactions[,"usersha1id"]), "transactions")
@@ -12,4 +9,7 @@ itemFrequencyPlot(transactions, topN=40)
 rules = apriori(transactions, parameter = list(support = 0.001,confidence = 0.01, maxtime = 1000))
 
 inspect(rules[1:10])
+inspect(sort(rules, by = "lift")[1:20])
 
+pink_floyd_rules = subset(rules, items %in% c("pink floyd","roger waters"))
+inspect(sort(pink_floyd_rules, by = "lift")[1:40])
