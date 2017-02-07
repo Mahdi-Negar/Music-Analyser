6e14d8915a46bb34e8b2220b0ccf5fde722b83e9
Negar Mokhberian <negar.mokhberian@gmail.com>
2017-02-07 18:44:01 +0330

    behtar shod

--- association.R
+++ association.R
@@ -6,8 +6,10 @@ map_to_integer <- function(v){
   as.integer(factor(v, levels = unique(v)))
 }
 
-data.sparse = sparseMatrix(map_to_integer(data_360k[1:100000,]$usersha1id), map_to_integer(data_360k[1:100000,]$mbartid), x=data_360k[1:100000,]$plays)
-conv.data.sparse = as(data.sparse, "ngCMatrix")
+transactions = as.data.frame(data_360k[,c(1,3)])
+transactions <- as(split(transactions[,"artname"], transactions[,"usersha1id"]), "transactions")
+itemFrequencyPlot(transactions, topN=40)
+rules = apriori(transactions, parameter = list(support = 0.001,confidence = 0.01, maxtime = 1000))
 
-rules = apriori(conv.data.sparse, parameter = list(support = 0.001,confidence = 0.01))
+inspect(rules[1:10])
 
